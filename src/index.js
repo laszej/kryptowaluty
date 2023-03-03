5f0df875a66ace171795f056074da275a367c0c6
laszej <stanislawlaskowski87@gmail.com>
2023-03-03 19:26:48 +0100

    lupa

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HashRouter as Router, Switch, Route,} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ 
   <React.StrictMode>
+     <Router>
     <App />
+    </Router>
   </React.StrictMode>
+  
 );
 
 
